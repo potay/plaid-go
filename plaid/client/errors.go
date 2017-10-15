5686f836ab72dffb4c9e5651606301e2d5906086
Paul Chun <me@paulchun.com>
2017-10-15 13:44:07 -0700

    Update error handling

--- plaid/client/errors.go
+++ plaid/client/errors.go
@@ -5,16 +5,17 @@ import (
 )
 
 type plaidClientError struct {
-	// List of all errors: https://github.com/plaid/support/blob/master/errors.md
-	ErrorCode int    `json:"code"`
-	Message   string `json:"message"`
-	Resolve   string `json:"resolve"`
+	// List of all errors: https://plaid.com/docs/api/#errors-overview
+	ErrorType      string `json:"error_type"`
+	ErrorCode      string `json:"error_code"`
+	ErrorMessage   string `json:"error_message"`
+	DisplayMessage string `json:"display_message"`
 
 	// StatusCode needs to manually set from the http response
 	StatusCode int
 }
 
 func (e plaidClientError) Error() string {
-	return fmt.Sprintf("Plaid Error - http status: %d, code: %d, message: %s, resolve: %s",
-		e.StatusCode, e.ErrorCode, e.Message, e.Resolve)
+	return fmt.Sprintf("Plaid Error - http status: %d, type: %s, code: %s, message: %s, resolve: %s",
+		e.StatusCode, e.ErrorType, e.ErrorCode, e.ErrorMessage, e.DisplayMessage)
 }
